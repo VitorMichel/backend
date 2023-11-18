8ea42825b906bf528101a30c2d55cc0d75f2a866
DESKTOP-8E9KFQB\VITOR <vitor.michel99@gmail.com>
2023-11-18 13:23:41 -0300

    adicionada integracao ao kafka no controller

--- src/controllers/pagamentoController.js
+++ src/controllers/pagamentoController.js
@@ -1,17 +1,32 @@
-// Simulando um banco de dados simples
-let pagamentos = [];
+const { producer, consumer, connectKafka, disconnectKafka } = require('../kafka');
 
 module.exports = {
-  buscarPagamentos: (req, res) => {
+  buscarPagamentos: async (req, res) => {
+    // Conectar ao Kafka
+    await connectKafka();
+
+    // Lógica para buscar pagamentos do Kafka
+
+    // Desconectar do Kafka
+    await disconnectKafka();
+
     res.json(pagamentos);
   },
 
-  realizarPagamento: (req, res) => {
+  realizarPagamento: async (req, res) => {
     const { valor, descricao } = req.body;
 
     if (!valor || !descricao) {
       return res.status(400).json({ mensagem: 'Valor e descrição são obrigatórios.' });
     }
+
+    // Conectar ao Kafka
+    await connectKafka();
+
+    // Lógica para enviar pagamento ao Kafka
+
+    // Desconectar do Kafka
+    await disconnectKafka();
 
     const pagamento = {
       id: pagamentos.length + 1,
