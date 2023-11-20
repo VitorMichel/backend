185ac86999c0fddbaafd80f77800d20596244ed1
Henrique Menna Barreto <henrique.menna@cloudwalk.io>
2023-11-20 12:05:51 -0300

    payment producer async connect

---
+++ src/kafka/payment.js
@@ -0,0 +1,41 @@
+const kafka = require('./kafka');
+const broker = kafka.start();
+const producer = broker.producer({});
+const topic = "payment-order";
+
+(async () => {
+    await producer.connect();
+})();
+
+const produce = async () => {
+
+    let i = 0
+
+    // after the produce has connected, we start an interval timer
+    setInterval(async () => {
+        try {
+            // send a message to the configured topic with
+            // the key and value formed from the current value of `i`
+            await producer.send({
+                topic,
+                acks: 1,
+                messages: [
+                    {
+                        key: String(i),
+                        value: "this is message " + i,
+                    },
+                ],
+            })
+
+            // if the message is written successfully, log it and increment `i`
+            console.log("writes: ", i)
+            i++
+        } catch (err) {
+            console.error("could not write message " + err)
+        }
+    }, 1000)
+}
+
+module.exports = {
+    produce
+}
