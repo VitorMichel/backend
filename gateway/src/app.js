3b24b650371756c3ea70e2cdfb92a1b3d6c957f4
Henrique Menna Barreto <henrique.menna@cloudwalk.io>
2023-11-20 17:31:01 -0300

    redis

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -2,6 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const pagamentoRoutes = require('./routes/pagamentoRoutes');
 
+(async () => {
+
+  const redis = require('redis');
+  const client = redis.createClient();
+  client.connect();
+
+  client.on("error", (error) => {
+    console.error(error);
+  });
+
+  const result = await client.set("key", "value");
+  console.log(`result: ${result}`);
+
+  const result2 = await client.get("key");
+  console.log(`result2: ${result2}`);
+
+})();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
