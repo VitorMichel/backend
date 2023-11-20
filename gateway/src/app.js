124dc327963af58d9f2cc2667e4c1aaee2de8ff0
Henrique Menna Barreto <henrique.menna@cloudwalk.io>
2023-11-20 17:32:22 -0300

    fix path

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -15,9 +15,6 @@ const pagamentoRoutes = require('./routes/pagamentoRoutes');
   const result = await client.set("key", "value");
   console.log(`result: ${result}`);
 
-  const result2 = await client.get("key");
-  console.log(`result2: ${result2}`);
-
 })();
 
 const app = express();
